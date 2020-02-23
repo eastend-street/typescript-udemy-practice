445aa424a0e25fbb6b834b2daf326797c01d32ba
eastend-street <ss_1322irf@icloud.com>
2020-02-22 17:08:25 -0800

    literal

---
+++ src/leteral.ts
@@ -0,0 +1,14 @@
+export {};
+
+// literal型を使う時　例）特定の文字列のみ許容したい時。曜日のみ許容したい
+let dayOfTheWeek: "Sun" | "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" = "Sun";
+dayOfTheWeek = "Mon";
+// dayOfTheWeek = "Hello";
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 12;
+// month = 13;
+
+// booleanはもともと有限なのでほぼ使わない
+let TRUE: true = true;
+TRUE = false;
